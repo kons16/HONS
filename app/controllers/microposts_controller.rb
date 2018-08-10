18bf608765b3a478383b29c830b84a014f4d41b8
kons16 <ks1216r@gmail.com>
2018-08-10 06:35:25 +0000

    db

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,7 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only:[:new, :create, :destroy]
   before_action :correct_user, only: :destroy
-  
+   
   def index
     @microposts = Micropost.rank(:row_order)
   end
